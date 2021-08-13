b00b7d45662eb4809e7bc00782e26666b754008a
preetas <88144614+preetas@users.noreply.github.com>
2021-08-13 11:41:32 +0530

    Update IndexController.java

--- spring-boot-demo/src/main/java/com/howtodoinjava/app/controller/IndexController.java
+++ spring-boot-demo/src/main/java/com/howtodoinjava/app/controller/IndexController.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.boot.SpringApplication
+import org.springframework.boot.SpringApplication;
 @Controller
 public class IndexController {
 
